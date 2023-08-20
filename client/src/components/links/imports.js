d42a802171d61966042fdc6ce4bf32208533f461
Simonn591 <simon.marchal@seznam.cz>
2023-08-20 11:48:11 +0200

    remove: images import

--- client/src/components/links/imports.js
+++
@@ -1,8 +0,0 @@
-import mailDark from '../../assets/mail-dark.png';
-import linkedinDark from '../../assets/linkedin-dark.png';
-import gitDark from '../../assets/git-dark.png';
-import mailLight from '../../assets/mail-light.png';
-import linkedinLight from '../../assets/linkedin-light.png';
-import gitLight from '../../assets/git-light.png';
-
-export { mailDark, linkedinDark, gitDark, mailLight, linkedinLight, gitLight };
